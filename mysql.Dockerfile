bdf34b628bed33d939cade54bd501f2630813510
Jether Rodrigues do Nascimento <jetherrodrigues@gmail.com>
2019-09-27 14:55:28 -0300

    allowing jpa ddl auto true

--- mysql.Dockerfile
+++ mysql.Dockerfile
@@ -1,6 +1,6 @@
 FROM mysql:5.7
 
-ENV MYSQL_DATABASE=users \
+ENV MYSQL_DATABASE=picpay \
     MYSQL_ROOT_PASSWORD=root
 
 ADD ./support/db-init.sql /docker-entrypoint-initdb.d/
--- src/main/java/com/picpay/users/UsersApplication.java
+++ src/main/java/com/picpay/users/UsersApplication.java
@@ -1,9 +1,13 @@
 package com.picpay.users;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EnableAutoConfiguration
+@EntityScan(basePackages = {"com.picpay.users.model"})
 public class UsersApplication {
 
     public static void main(String[] args) {
